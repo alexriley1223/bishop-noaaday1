6346d29b84baebddf2e64f39ac0bc8050d2ed009
Alex Riley <alexriley1223@gmail.com>
2023-08-18 17:08:14 -0400

    fix: updated name and description

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
 module.exports = function() {
 	const module = {};
 
-	module.name = 'Bishop Weather';
-	module.description = 'Weather commands for Bishop.';
-	module.version = '1.0.0';
+	module.name = 'Bishop NOAA Day 1';
+	module.description = 'Post the NOAA NWS Day One prediction each morning on the Bishop Discord bot';
+	module.version = '1.0.1';
 	module.enabled = true;
 
 	module.init = function init() {
